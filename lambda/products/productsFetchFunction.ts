d1135d0d41d48757f0944946f5e645e285ef7552
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2025-05-27 14:50:08 -0300

    Modified infos and new returns

--- lambda/products/productsFetchFunction.ts
+++ lambda/products/productsFetchFunction.ts
@@ -1,10 +1,30 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
 
 export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-  if(event.resource === "/products") {
-    if (event.httpMethod === 'GET') {
-      
+
+  const lambdaRequestId = context.awsRequestId; // Gerado no AWS Lambda a cada execução da Lambda. Você encontra o ID no CloudWatch.
+  const apiRequestId = event.requestContext.requestId; // Esse é chamado da API Gateway, ele é um ID ÚNICO/EXCLUSIVO e ajuda a rastrear cada requisição HTTP.
+  const method = event.httpMethod;
+
+  console.log(`AWS LAMBDA: ${lambdaRequestId} - API GATEWAY: ${apiRequestId}`);
+  
+  if (event.resource === "/products") {
+    if (method === 'GET') {
+      console.log('GET');
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: 'GET Products  - OK',
+        })
+      }
     }
   }
 
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: 'Bad Request',
+    })
+  }
 }
