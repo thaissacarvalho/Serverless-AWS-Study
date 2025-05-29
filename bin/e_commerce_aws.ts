e626010b3a4c4d5e2b48c7126ed6235257081bc8
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2025-05-29 11:17:40 -0300

    add Product Layer and dependency

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ProductsAppStack } from "../lib/productApp-stack";
 import { EcommerceAPIStack } from "../lib/ecommerceApi-stack";
+import { ProductsAppLayersStack } from '../lib/productAppLayers-stack';
 
 const app = new cdk.App();
 
@@ -16,13 +17,20 @@ const tags = {
   team: "SiecolaCode",
 };
 
+const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayers", {
+  tags: tags,
+  env: env
+});
+
 const productsAppStack = new ProductsAppStack(app, "ProductsApp", {
   tags: tags,
   env: env
 });
 
+productsAppStack.addDependency(productsAppLayersStack);
+
 const eCommerceAPIStack = new EcommerceAPIStack(app, "EcommerceApp", {
-  productsFetchHandler: productsAppStack.productFetchHandler, 
+  productsFetchHandler: productsAppStack.productFetchHandler,
   productsAdminHandler: productsAppStack.productAdminHandler,
   tags: tags,
   env: env
